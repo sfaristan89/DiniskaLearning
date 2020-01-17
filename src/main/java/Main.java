e0d3e2964bf9f6f8866f733426192d5a0a763121
sfaristan89 <tester.trolo01@gmail.com>
2020-01-17 14:45:03 +0200

    loop

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        for (int i = 10; i > 0; i--){
+        for (int i = 10; i > 0; i--) {
             System.out.println(i);
         }
 
