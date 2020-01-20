128f2d26a5d1f1fcfe4d63ffe681cc4c1f2fe250
sfaristan89 <tester.trolo01@gmail.com>
2020-01-20 22:40:39 +0200

    phone numbers

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,6 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -11,9 +14,16 @@ public class Main {
         System.out.println();
         System.out.println(multiplying(2,3));
         System.out.println();
-        dividing(10,0);
+        dividing(19,2);
         System.out.println();
-        divByModule(10,0);
+        divByModule(19,2);
+        System.out.println();
+        String phoneNumber = "+380674739224";
+        if (isValidE123(phoneNumber)) {
+            System.out.print(phoneNumber + " is valid phone number");
+        }else {
+            System.out.print(phoneNumber + " is invalid Phone number");
+        }
 
     }
 
@@ -64,5 +74,13 @@ public class Main {
         } catch (ArithmeticException e){
             System.out.println("Division by zero");
         }
+    }
+
+    public static boolean isValidE123(String s)
+    {
+        Pattern p = Pattern.compile("^(?:(?:\\+|0{0,2})((38)|8?))(0(39|50|6[3678]|73|9[1-9])\\d{7})$");
+
+        Matcher m = p.matcher(s);
+        return (m.find() && m.group().equals(s));
     }
 }
